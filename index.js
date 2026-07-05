24175a6982c67f5816552c7b7d7286e1e853dcca
Mohamed Marghoub <mohamedmarghoub0726@gmail.com>
2026-07-05 19:52:10 +0100

    Implement the renderCard function logic

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const cards = document.querySelector(".cards");
+const cardTemplate = document.querySelector("#card-template");
 
 function Book(id, title, author, read) {
     this.id = id;
@@ -8,4 +10,25 @@ function Book(id, title, author, read) {
 
 Book.prototype.toggleRead = function() {
     this.read = !this.read;
+}
+
+function renderCard(book) {
+    const card = cardTemplate.content.firstElementChild.cloneNode(true);
+    
+    const title = card.querySelector(".card__title");
+    title.textContent = book.title;
+
+    const author = card.querySelector(".card__author");
+    author.textContent = book.author;
+
+    const checkbox = card.querySelector(".toggle__btn");
+    checkbox.id = `read-${book.id}`;
+    checkbox.checked = book.read;
+
+    const label = card.querySelector(".toggle__label");
+    label.htmlFor = checkbox.id;
+
+    checkbox.addEventListener("click", book.toggleRead);
+
+    cards.appendChild(card);
 }
